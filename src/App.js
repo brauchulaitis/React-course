2c10614cf6687ad1867313c17bbee897fbc46ab6
Oskars Braucs <oskars.braucs@gmail.com>
2024-05-09 12:58:37 +0300

    Excersise 4°

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import Heading from './Heading';
+import Card from "./card";
 
 function App() {
   return (
-      <div className="App">
-          <Heading firstName="Bob" />
-          <Heading firstName="Alice" />
-      </div>
+    <div>
+      <h1>Task: Add three Card elements</h1>
+      <Card h2="First card's h2" h3="First card's h3" />
+      <Card h2="Second card's h2" h3="Second card's h3" />
+      <Card h2="Third card's h2" h3="Third card's h3" />
+    </div>
   );
-};
+}
 
 export default App;
+
 
