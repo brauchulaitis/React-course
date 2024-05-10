d1c73ec6fdd3fa2f159a95ca349950d490699e89
Oskars Braucs <oskars.braucs@gmail.com>
2024-05-10 11:33:30 +0300

    Excercise 4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import Card from "./card";
+import React from 'react';
 
 function App() {
+  function handleClick() {
+    let randomNum = Math.floor(Math.random() * 3) + 1;
+    console.log(randomNum);
+    let userInput = prompt('type a number');
+    alert(`Computer number: ${randomNum}, Your guess: ${userInput}`);
+  }
+
   return (
     <div>
-      <h1>Task: Add three Card elements</h1>
-      <Card h2="First card's h2" h3="First card's h3" />
-      <Card h2="Second card's h2" h3="Second card's h3" />
-      <Card h2="Third card's h2" h3="Third card's h3" />
+      <h1>Task: Add a button and handle a click event</h1>
+      <button onClick={handleClick}>
+        Guess the number between 1 and 3
+      </button>
     </div>
   );
 }
